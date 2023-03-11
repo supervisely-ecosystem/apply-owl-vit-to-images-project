6122f6b99aeb4000ffa3d757c4e7d662cfb6bef8
ruslantau <ruslan042743@gmail.com>
2023-03-11 20:26:13 +0700

    Text inference was added

--- src/main.py
+++ src/main.py
@@ -1,91 +1,181 @@
+import os
+import sys
+from typing import List
+import random
+
+import jax
+import numpy as np
 import supervisely as sly
 from supervisely.app.widgets import (
-    Button, Card, Progress, Text, Empty,
-    Container, Checkbox, Stepper, ClassesTable, 
-    ImageRegionSelector, ProjectThumbnail,
-    RadioTabs, TextArea, GridGallery, InputNumber, Field, Progress
+    Button,
+    Card,
+    Progress,
+    Text,
+    Empty,
+    Container,
+    Checkbox,
+    Stepper,
+    ClassesTable,
+    ImageRegionSelector,
+    ProjectThumbnail,
+    RadioTabs,
+    TextArea,
+    GridGallery,
+    InputNumber,
+    Field,
+    Progress,
 )
-
 import src.sly_globals as g
-import sys 
-import os
-sys.path.append(os.path.join(g.app_root_directory, "scenic"))
 
+sys.path.append(os.path.join(g.app_root_directory, "scenic"))
 from scenic.projects.owl_vit import models
-from scenic.projects.owl_vit.configs import clip_b16 as config_module
-from scenic.projects.owl_vit.notebooks import inference
-# from scenic.projects.owl_vit.notebooks import interactive
-# from scenic.projects.owl_vit.notebooks import plotting
-from scenic.model_lib.base_models import box_utils
-import numpy as np
+from scenic.projects.owl_vit.configs import clip_b32
+from src.model import prepare_image, prepare_text, get_predictions, draw_predictions
+
+
+PREVIEW_IMAGES_INFOS = []
+datasets_list = g.api.dataset.get_list(g.project_id)
+image_info_list = []
+for dataset in datasets_list:
+    samples_count = (
+        dataset.images_count
+        if len(datasets_list) == 1
+        else dataset.count * (100 - len(datasets_list)) / 100
+    )
+    image_info_list += random.sample(g.api.image.get_list(dataset.id), samples_count)
+    if len(image_info_list) >= 1000:
+        break
+
+ref_image_info = image_info_list[0]
+
+
+def get_image_path(image_name: str) -> str:
+    for dataset_name, dataset in g.project_fs.datasets:
+        if dataset.item_exists(image_name):
+            return dataset.get_img_path(image_name)
+
 
 ######################
 ### Input project card
 ######################
-project_preview = ProjectThumbnail(g.project)
+project_preview = ProjectThumbnail(g.project_info)
 progress_bar_download_data = Progress(hide_on_finish=False)
 progress_bar_download_data.hide()
-text_download_data = Text('Data has been successfully downloaded', status='success')
+text_download_data = Text("Data has been successfully downloaded", status="success")
 text_download_data.hide()
-button_download_data = Button('Download')
+button_download_data = Button("Download")
+
+
 @button_download_data.click
 def download_data():
     try:
         if sly.fs.dir_exists(g.project_dir):
-            sly.logger.info('Data already downloaded.')
+            sly.logger.info("Data already downloaded.")
         else:
             button_download_data.hide()
             progress_bar_download_data.show()
             sly.fs.mkdir(g.project_dir)
-            with progress_bar_download_data(message=f"Processing images...", total=g.project.items_count) as pbar:
+            with progress_bar_download_data(
+                message=f"Processing images...", total=g.project.items_count
+            ) as pbar:
                 sly.Project.download(
-                    api=g.api, 
-                    project_id=g.project_id, 
+                    api=g.api,
+                    project_id=g.project_id,
                     dest_dir=g.project_dir,
                     batch_size=100,
                     progress_cb=pbar.update,
-                    only_image_tags=False, 
-                    save_image_info=True)
-            sly.logger.info('Data successfully downloaded.')
+                    only_image_tags=False,
+                    save_image_info=True,
+                )
+            sly.logger.info("Data successfully downloaded.")
         g.project_fs = sly.Project(g.project_dir, sly.OpenMode.READ)
         progress_bar_download_data.hide()
         button_download_data.hide()
         text_download_data.show()
         stepper.set_active_step(2)
     except Exception as e:
-        sly.logger.info('Something went wrong.')
+        sly.logger.info("Something went wrong.")
         progress_bar_download_data.hide()
         button_download_data.show()
-        text_download_data.set('Data download failed', status='error')
+        text_download_data.set("Data download failed", status="error")
         text_download_data.show()
         stepper.set_active_step(1)
+
+
 data_card = Card(
-    title="Input data", 
-    content=Container([project_preview, progress_bar_download_data, text_download_data, button_download_data])
+    title="Input data",
+    content=Container(
+        [
+            project_preview,
+            progress_bar_download_data,
+            text_download_data,
+            button_download_data,
+        ]
+    ),
 )
 
 
 ###########################################
 ### Model settings and results preview card
 ###########################################
-text_prompt_textarea = TextArea(placeholder='Description of object, that you want to detect via NN model', rows=10)
-datasets_list = g.api.dataset.get_list(g.project_id)
-image_info_list = g.api.image.get_list(datasets_list[0].id)
-ref_image = image_info_list[0]
-image_region_selector = ImageRegionSelector(image_info=ref_image, widget_width='500px', widget_height='500px')
+text_prompt_textarea = TextArea(
+    placeholder="Description of object, that you want to detect via NN model", rows=10
+)
+image_region_selector = ImageRegionSelector(
+    image_info=ref_image_info, widget_width="500px", widget_height="500px"
+)
+
+
 @image_region_selector.bbox_changed
 def bbox_updated(new_scaled_bbox):
     sly.logger.info(f"new_scaled_bbox: {new_scaled_bbox}")
 
-previous_image_button = Button('Previous image', icon='zmdi zmdi-skip-previous')
-next_image_button = Button('Next image', icon='zmdi zmdi-skip-next')
-random_image_button = Button('New random image', icon='zmdi zmdi-refresh')
-set_input_button = Button('Set model input')
+
+previous_image_button = Button("Previous image", icon="zmdi zmdi-skip-previous")
+next_image_button = Button("Next image", icon="zmdi zmdi-skip-next")
+random_image_button = Button("New random image", icon="zmdi zmdi-refresh")
+set_input_button = Button("Set model input")
+
+
+@set_input_button.click
+def set_model_input():
+    if model_settings_card.is_disabled() is False:
+        model_settings_card.disable()
+        model_input_tabs.disable()
+        previous_image_button.disable()
+        next_image_button.disable()
+        random_image_button.disable()
+        image_region_selector.disable()
+        text_prompt_textarea.disable()
+        set_input_button.text = "Change model input"
+    else:
+        model_settings_card.enable()
+        model_input_tabs.enable()
+        previous_image_button.enable()
+        next_image_button.enable()
+        random_image_button.enable()
+        image_region_selector.enable()
+        text_prompt_textarea.enable()
+        set_input_button.text = "Set model input"
+
+
 model_input_tabs = RadioTabs(
-    titles=['Reference image', 'Text prompt'], 
-    contents=[Container([image_region_selector, previous_image_button, next_image_button, random_image_button]),
-              Container([text_prompt_textarea])], 
-    descriptions=["Pick object by bounding box editing", "Describe object, that you want to detect"]
+    titles=["Reference image", "Text prompt"],
+    contents=[
+        Container(
+            [
+                image_region_selector,
+                previous_image_button,
+                next_image_button,
+                random_image_button,
+            ]
+        ),
+        Container([text_prompt_textarea]),
+    ],
+    descriptions=[
+        "Pick object by bounding box editing",
+        "Describe object, that you want to detect",
+    ],
 )
 model_settings_card = Card(
     title="Model settings",
@@ -93,98 +183,105 @@ model_settings_card = Card(
     content=Container([model_input_tabs, set_input_button]),
 )
 
-run_model_button = Button('Run model')
 grid_gallery = GridGallery(
-    columns_number=3,
+    columns_number=g.PREVIEW_IMAGES_COUNT,
     annotations_opacity=0.5,
     show_opacity_slider=True,
     enable_zoom=False,
     sync_views=False,
     fill_rectangle=True,
 )
-
-def get_random_image(image_infos_list):
-    from random import choice
-    return choice(image_infos_list)
-
-field_confidence_threshhold = Field(title='Confidence threshold', description='Threshold for the minimum confidence that a detection must have to be displayed (higher values mean fewer boxes will be shown):', content=InputNumber(value=0.5, min=0.1, max=1, step=0.1))
-field_nms_threshhold = Field(title='NMS threshold', description='Threshold for non-maximum suppression of overlapping boxes (higher values mean more boxes will be shown)', content=InputNumber(value=0.5, min=0.1, max=1, step=0.1))
-for i in range(6):
-    img_info = get_random_image(image_info_list)
+for i in range(g.PREVIEW_IMAGES_COUNT):
+    img_info = random.choice(image_info_list)
+    PREVIEW_IMAGES_INFOS.append(img_info)
     grid_gallery.append(
-        title=img_info.name, 
-        image_url=img_info.full_storage_url, 
-        # annotation=img_info.ann, 
-        column_index=int(i % 3)
+        title=img_info.name,
+        image_url=img_info.preview_url,
+        column_index=int(i % 3),
     )
-update_preview_button = Button('Update preview')
+confidence_threshhold_input = InputNumber(value=0.5, min=0.1, max=1, step=0.1)
+nms_threshhold_input = InputNumber(value=0.5, min=0.1, max=1, step=0.1)
+field_confidence_threshhold = Field(
+    title="Confidence threshold",
+    description="Threshold for the minimum confidence that a detection must have to be displayed (higher values mean fewer boxes will be shown):",
+    content=confidence_threshhold_input,
+)
+field_nms_threshhold = Field(
+    title="NMS threshold",
+    description="Threshold for non-maximum suppression of overlapping boxes (higher values mean more boxes will be shown)",
+    content=nms_threshhold_input,
+)
+update_preview_button = Button("Update preview")
+
+
 @update_preview_button.click
 def update_preview():
-    config = config_module.get_config(init_mode='canonical_checkpoint')
+    confidence_threshhold = confidence_threshhold_input.get_value()
+    nms_threshhold = nms_threshhold_input.get_value()
+
+    config = clip_b32.get_config(init_mode="canonical_checkpoint")
     module = models.TextZeroShotDetectionModule(
         body_configs=config.model.body,
         normalize=config.model.normalize,
-        box_bias=config.model.box_bias)
+        box_bias=config.model.box_bias,
+    )
     variables = module.load_variables(config.init_from.checkpoint_path)
-    model = inference.Model(config, module, variables)
-    model.warm_up()
+    # model = inference.Model(config, module, variables)
+    # model.warm_up()
 
-    PREVIEW_IMAGES_COUNT = 6
-    IMAGE_COND_MIN_CONF = 0.5
-    IMAGE_COND_NMS_IOU_THRESHOLD = 0.5
+    query_image = sly.image.read(get_image_path(ref_image_info))
 
-    images_infos, _ = g.project_fs.get_train_val_splits_by_count(g.project_dir, train_count=PREVIEW_IMAGES_COUNT, val_count=0)
-    query_image = sly.image.read(ref_image.img_path)
-    
-    x0,y0,x1,y1 = np.array(image_region_selector.scaled_bbox).reshape(-1)
-    box = [y1,x0,y0,x1]
-    query_embedding, best_box_ind = model.embed_image_query(query_image, box)
+    x0, y0, x1, y1 = np.array(image_region_selector.scaled_bbox).reshape(-1)
+    box = [y1, x0, y0, x1]
+    query_embedding, best_box_ind = module.embed_image_query(query_image, box)
 
-    for image_info in images_infos:
-        target_image = sly.image.read(image_info.img_path)
-        # TODO(mjlm): Implement multi-query image-conditioned detection.
-        num_queries = 1
-        top_query_ind, scores = model.get_scores(target_image, query_embedding[None, ...], num_queries=1)
-        
-        # Apply non-maximum suppression:
-        if IMAGE_COND_NMS_IOU_THRESHOLD < 1.0:
-            _, _, target_image_boxes = model.embed_image(target_image)
-            target_boxes_yxyx = box_utils.box_cxcywh_to_yxyx(target_image_boxes, np)
-            for i in np.argsort(-scores):
-                if not scores[i]:
-                    # This box is already suppressed, continue:
-                    continue
-                ious = box_utils.box_iou(
-                    target_boxes_yxyx[None, [i], :],
-                    target_boxes_yxyx[None, :, :],
-                    np_backbone=np)[0][0, 0]
-                ious[i] = -1.0  # Mask self-IoU.
-                scores[ious > IMAGE_COND_NMS_IOU_THRESHOLD] = 0.0
+    for i, image_info in enumerate(PREVIEW_IMAGES_INFOS):
+        target_image = sly.image.read(get_image_path(image_info))
+        input_image = prepare_image(target_image)
 
-        for bbox, confidence in zip(target_boxes_yxyx, scores):
-            if confidence > IMAGE_COND_MIN_CONF:
-                grid_gallery._data[i]
-                
-    if model_input_tabs.get_active_tab() == 'Reference image':
-        selected_bbox = image_region_selector.get_relative_coordinates()
-    else:
-        text_prompt = text_prompt_textarea.get_value()
-    
+        if model_input_tabs.get_active_tab() == "Reference image":
+            selected_bbox = image_region_selector.get_relative_coordinates()
+        else:
+            text_prompt = text_prompt_textarea.get_value().split(";")
+            tokenized_queries = prepare_text(text_prompt)
+            # Note: The model expects a batch dimension.
+            predictions = module.apply(
+                variables,
+                input_image[None, ...],
+                tokenized_queries[None, ...],
+                train=False,
+            )
 
-    
-    
-    pass
+            # Remove batch dimension and convert to numpy:
+            predictions = jax.tree_util.tree_map(lambda x: np.array(x[0]), predictions)
+
+        draw_predictions(predictions, confidence_threshhold, nms_threshhold)
+        sly.logger.info(
+            f"{i+1} image processed. {len(PREVIEW_IMAGES_INFOS) - (i+1)} images left."
+        )
+
+
 preview_card = Card(
     title="Preview results",
     description="Model prediction result preview",
-    content=Container([field_confidence_threshhold, field_nms_threshhold, grid_gallery, update_preview_button]),
+    content=Container(
+        [
+            field_confidence_threshhold,
+            field_nms_threshhold,
+            grid_gallery,
+            update_preview_button,
+        ]
+    ),
 )
 
-model_progress = Progress(message='Applying model..', hide_on_finish=False)
+run_model_button = Button("Run model")
+model_progress = Progress(message="Applying model..", hide_on_finish=False)
 run_model_card = Card(
     title="Model apply progress",
     content=Container([run_model_button, model_progress]),
 )
 
-stepper = Stepper(widgets=[data_card, model_settings_card, preview_card, run_model_card])
+stepper = Stepper(
+    widgets=[data_card, model_settings_card, preview_card, run_model_card]
+)
 app = sly.Application(layout=stepper)
--- src/sly_globals.py
+++ src/sly_globals.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 import supervisely as sly
 from dotenv import load_dotenv
+
 load_dotenv("local.env")
 load_dotenv(os.path.expanduser("~/supervisely.env"))
 
@@ -12,16 +13,17 @@ app_root_directory = str(Path(__file__).parent.absolute().parents[0])
 sly.logger.info(f"Root source directory: {app_root_directory}")
 project_dir = os.path.join(app_root_directory, "sly_project")
 data_dir = os.path.join(app_root_directory, "data")
-checkpoints_dir = os.path.join(data_dir, 'checkpoints')
+checkpoints_dir = os.path.join(data_dir, "checkpoints")
 os.environ["SLY_APP_DATA_DIR"] = data_dir
 Path(checkpoints_dir).mkdir(exist_ok=True, parents=True)
 
 api = sly.Api()
-project_id = sly.env.project_id()
 project_fs: sly.Project = None
+project_id = sly.env.project_id()
+project_info = api.project.get_info_by_id(project_id)
 project_meta = sly.ProjectMeta.from_json(api.project.get_meta(project_id))
-
-project = api.project.get_info_by_id(project_id)
-workspace = api.workspace.get_info_by_id(project.workspace_id)
+workspace = api.workspace.get_info_by_id(project_info.workspace_id)
 # team.id will be used for storaging app results in team files
 team = api.team.get_info_by_id(workspace.team_id)
+
+PREVIEW_IMAGES_COUNT = 6
