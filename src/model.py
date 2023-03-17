41af0329361dedf473cbab4fdd122c21dd4f05e8
ruslantau <ruslan042743@gmail.com>
2023-03-17 19:57:21 +0700

    Experimant text inference was integrated

--- src/model.py
+++ src/model.py
@@ -52,3 +52,18 @@ def predictions_to_anno(scores, boxes, labels, image_info, confidence_threshhold
         obj_label = sly.Label(sly.Rectangle(y0, x0, y1, x1), obj_class)
         new_annotation = new_annotation.add_label(obj_label)
     return new_annotation
+
+def inference_json_anno_preprocessing(ann, project_meta: sly.ProjectMeta) -> sly.Annotation:
+    temp_meta = project_meta.clone()
+    pred_classes = []
+    for i, obj in enumerate(ann['annotation']['objects']):
+        class_ = obj['classTitle'] + '_pred'
+        pred_classes.append(class_)
+        ann['annotation']['objects'][i]['classTitle'] = class_
+        
+        if temp_meta.get_obj_class(class_) is None:
+            new_obj_class = sly.ObjClass(class_, sly.Rectangle)
+            temp_meta = temp_meta.add_obj_class(new_obj_class)
+    if temp_meta.get_tag_meta('confidence') is None:
+        temp_meta = temp_meta.add_tag_meta(sly.TagMeta('confidence', sly.TagValueType.ANY_NUMBER))
+    return sly.Annotation.from_json(ann["annotation"], temp_meta)
